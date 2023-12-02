13c546a92f2f466205deb28775c91bcbcd3aad3d
Feldraas <feldraas@gmail.com>
2023-12-02 20:48:09 +0100

    Add Day 2 2023

---
+++ aoc23/src/day2.clj
@@ -0,0 +1,59 @@
+(ns aoc23.src.day2
+  (:require [utils :refer [read-input
+                           parse-int]]
+            [clojure.string :as str]))
+
+(defn parse-color
+  [color string]
+  (as-> color $
+        (str "(\\d+) " $)
+        (re-pattern $)
+        (re-seq $ string)
+        (if (nil? $)
+          0
+          (parse-int (last (first $))))))
+
+(defn parse-round
+  [round]
+  (map #(parse-color % round) ["red" "green" "blue"]))
+
+(defn parse-game
+  [game]
+  (as-> game $
+        (str/split $ #";")
+        (map parse-round $)))
+
+(defn round-possible?
+  [game limits]
+  (and
+    (<= (first game) (first limits))
+    (<= (second game) (second limits))
+    (<= (last game) (last limits))))
+
+(defn game-possible?
+  [game limits]
+  (->> game
+       (map #(round-possible? % limits))
+       (every? true?)))
+
+
+(def games (-> (read-input "day2.txt")))
+(def cube-limits [12 13 14])
+
+(as-> games $
+      (map parse-game $)
+      (map #(game-possible? % cube-limits) $)
+      (filter #(nth $ (dec %)) (range 1 (inc (count games))))
+      (apply + $))
+
+(defn minimum-amounts
+  [game]
+  (reduce #(list (max (first %1) (first %2))
+                 (max (second %1) (second %2))
+                 (max (last %1) (last %2))) game))
+
+(->> games
+     (map parse-game)
+     (map minimum-amounts)
+     (map #(apply * %))
+     (apply +))
