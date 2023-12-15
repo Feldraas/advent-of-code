55b6f7135190ba40ba053b0424ebdd5af574efa3
Feldraas <feldraas@gmail.com>
2023-12-15 14:51:43 +0100

    Fix example text regex

--- aoc_tools.clj
+++ aoc_tools.clj
@@ -1,6 +1,6 @@
 (ns aoc-tools
   (:require
-    [utils :refer [extract-numbers ->! unnil parse-int]]
+    [utils :refer [extract-numbers parse-int]]
     [clj-http.client :as client]
     [clojure.string :as str]
     [clojure.java.io :as io]
@@ -9,21 +9,21 @@
 (defn extract-example-text
   [body]
   (->> body
-       (re-seq #"(?is)<pre><code>(.*?)\n *?</code></pre>")
+       (re-seq #"(?is)<pre><code>(.*?)\n? *?</code></pre>")
        (first)
        (last)))
 
 (defn fetch-input
   [year day]
-  (let [base-url (str "https://adventofcode.com/20" year "/day/" day)
-        input-url (str base-url "/input")
-        cookie (slurp "token.txt")
-        payload {:headers {:Cookie     cookie
-                           :User-Agent "Feldraas' AoC Clojure Library https://github.com/Feldraas/advent-of-code"}}
-        base-response (client/get base-url payload)
-        example-text (extract-example-text (:body base-response))
+  (let [base-url       (str "https://adventofcode.com/20" year "/day/" day)
+        input-url      (str base-url "/input")
+        cookie         (slurp "token.txt")
+        payload        {:headers {:Cookie     cookie
+                                  :User-Agent "Feldraas' AoC Clojure Library https://github.com/Feldraas/advent-of-code"}}
+        base-response  (client/get base-url payload)
+        example-text   (extract-example-text (:body base-response))
         input-response (client/get input-url payload)
-        input-text (:body input-response)]
+        input-text     (:body input-response)]
     [(str/replace example-text "\n" "\r\n")
      (str/replace input-text "\n" "\r\n")]))
 
@@ -39,9 +39,9 @@
      (create-files (parse-int year) day)))
 
   ([year day]
-   (let [year (mod year 2000)
+   (let [year       (mod year 2000)
          input-path (str "aoc" year "/inputs/")
-         src-path (str "aoc" year "/src/")
+         src-path   (str "aoc" year "/src/")
          [example-text input-text] (fetch-input year day)]
      (do
        (spit (str input-path "day" day ".ex") example-text)
@@ -56,7 +56,7 @@
   []
   (let [[project _ file] (str/split (str *ns*) #"\.")
         year (first (extract-numbers project))
-        day (first (extract-numbers file))]
+        day  (first (extract-numbers file))]
     [year day]))
 
 (defn read-input
@@ -81,13 +81,13 @@
 (defn submit-answer
   [part answer]
   (let [[year day] (get-year-and-day)
-        url (f-str "https://adventofcode.com/20{year}/day/{day}/answer")
-        cookie (slurp "token.txt")
-        payload {:method      "POST"
-                 :headers     {:Cookie     cookie
-                               :User-Agent "Feldraas' AoC Clojure Library https://github.com/Feldraas/advent-of-code"}
-                 :form-params {"level" (str part) "answer" (str answer)}}
+        url      (f-str "https://adventofcode.com/20{year}/day/{day}/answer")
+        cookie   (slurp "token.txt")
+        payload  {:method      "POST"
+                  :headers     {:Cookie     cookie
+                                :User-Agent "Feldraas' AoC Clojure Library https://github.com/Feldraas/advent-of-code"}
+                  :form-params {"level" (str part) "answer" (str answer)}}
         response (client/post url payload)
-        _ (println "Submitting answer" answer)]
+        _        (println "Submitting answer" answer)]
     ;main (extract-main (:body response))]
     (re-seq #"^(.*answer.*)$" (:body response))))
