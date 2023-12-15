f00c17146ebb1c57fe6ff95b542ae1b405f7eeff
Feldraas <feldraas@gmail.com>
2023-12-15 20:20:12 +0100

    Add Day 15 2022

--- aoc22/src/day15.clj
+++ aoc22/src/day15.clj
@@ -1,80 +1,67 @@
 (ns aoc22.src.day15
   (:require
-    [aoc-tools :refer [read-input]]
-    [utils :refer [parse-int]]))
+    [aoc-tools :refer [read-input submit-answer]]
+    [utils :refer [extract-numbers]]))
 
-(def report (read-input))
+(def input (read-input :test))
+(def real-input (read-input))
 
 (defn distance
   [[x1 y1] [x2 y2]]
   (+ (abs (- x1 x2)) (abs (- y1 y2))))
 
-(def zone (as-> report $
-                (map #(re-seq #"-?\d+" %) $)
-                (map #(map parse-int %) $)
-                (map #(partition 2 %) $)
-                (reduce #(let [sensor (first %2)
-                               beacon (second %2)]
-                           (-> %1
-                               (assoc-in [:grid sensor] "S")
-                               (assoc-in [:grid beacon] "B")
-                               (assoc-in [:distances sensor] (distance sensor beacon))))
-                        {} $)))
+(defn get-sensors
+  [input]
+  (->> input
+       (map extract-numbers)
+       (map #(partition 2 %))
+       (map vec)
+       (map #(vector (first %) (distance (first %) (second %))))
+       (into {})))
+
+(defn row-coverage-from-sensor
+  [row [[x y] radius]]
+  (let [row-distance (abs (- row y))
+        col-distance (- radius row-distance)]
+    (if (neg? col-distance)
+      []
+      [(- x col-distance) (+ x col-distance)])))
+
+(defn row-coverage
+  [row sensors]
+  (->> sensors
+       (reduce #(conj %1 (row-coverage-from-sensor row %2)) [])
+       (remove empty?)
+       (sort-by first)))
+
+(defn find-hole-in-coverage
+  [row max-range [r1 r2 & more]]
+  (cond
+    (pos? (first r1)) [0 row]
+    (> (first r2) (inc (second r1))) [(inc (second r1)) row]
+    (> (second r2) max-range) (if (= row 2000000)
+                                (do (prn (- (second r2) (first r1))) nil ; Part 1
+                                    nil))
+    :else (find-hole-in-coverage row max-range (conj more [(first r1) (max (second r1) (second r2))]))))
+
+(defn find-beacon
+  [input max-range]
+  (let [sensors    (get-sensors input)
+        beacon-row (first (filter (fn [row] (->> sensors
+                                                 (row-coverage row)
+                                                 (find-hole-in-coverage row max-range)))
+                                  (range max-range)))]
+    (find-hole-in-coverage beacon-row max-range (row-coverage beacon-row sensors))))
+
+(defn frequency
+  [input max-range]
+  (let [[x y] (find-beacon input max-range)]
+    (+ (* 4000000 x) y)))
+
+(->> (frequency real-input 4000000)
+     time
+     #_(submit-answer 2))
 
 
 
-(def borders
-  (as-> (:grid zone) $
-        (keys $)
-        [(map first $) (map second $)]
-        (map (juxt #(apply min %) #(apply max %)) $)))
 
-(def xmin (first (first borders)))
-(def xmax (second (first borders)))
-(def ymin (first (second borders)))
-(def ymax (second (second borders)))
-
-(defn draw-zone
-  ([zone]
-   (draw-zone zone 0))
-  ([zone padding]
-   (->> (for [y (range 0 21)
-              x (range 0 21)]
-          (get-in zone [:grid [x y]] "."))
-        (partition 21)
-        (map #(apply str %)))))
-
-;(draw-zone zone)
-
-
-(defn in-radius
-  [r [x y]]
-  (for [dx (range (- r) (inc r))
-        dy (range (- (abs dx) r) (- (inc r) (abs dx)))
-        :when (not= [dx dy] [0 0])]
-    [(+ x dx) (+ y dy)]))
-
-
-(defn in-radius2
-  [r [x y] row]
-  (let [diff (abs (- y row))]
-    (for [dx (range (- diff r) (inc (- r diff)))]
-      [(+ x dx) row])))
-
-(defn eliminate-for-sensor
-  [zone sensor row]
-  (reduce #(update-in %1 [:grid %2] (fn [x] (or x "#")))
-          zone
-          (in-radius2 (get-in zone [:distances sensor]) sensor row)))
-
-(defn eliminate-all
-  [zone row]
-  (reduce #(eliminate-for-sensor %1 %2 row) zone (keys (:distances zone))))
-
-
-(def row 1)
-(time (def zzone (eliminate-all zone row)))
-(time (->> (for [x (range 4000000)]
-             [x row])
-           (remove #(get-in zzone [:grid %]))
-           (count)))
