22968f9b2e2a77812f4ec5a39a9d8097328e559d
Feldraas <feldraas@gmail.com>
2023-12-19 13:33:06 +0100

    Add Day 19 2023 (Part 2)

--- aoc23/src/day19.clj
+++ aoc23/src/day19.clj
@@ -1,7 +1,7 @@
 (ns aoc23.src.day19
   (:require
     [aoc-tools :refer [read-input submit-answer]]
-    [utils :refer [extract-numbers parse-int iterate-until]]
+    [utils :refer [extract-numbers parse-int iterate-until ->!]]
     [clojure.string :as str]))
 
 (def input (read-input :test))
@@ -70,3 +70,64 @@
      total-rating
      time
      #_(submit-answer 1))
+
+(defn merge*
+  [m1 m2]
+  (-> (merge (dissoc m1 "A") (dissoc m2 "A"))
+      (assoc "A" (concat (get m1 "A") (get m2 "A")))))
+
+(defn split-by-rules
+  [intervals rules]
+  (if (= (count rules) 1)
+    {(first rules) intervals}
+    (let [rule              (first rules)
+          ctg               (first rule)
+          op                (second rule)
+          [_ n next-flow] (first (re-seq #"(\d+):(\w+)" rule))
+          n                 (parse-int n)
+          matches           (update intervals ctg (if (= op \>)
+                                                    #(vector (inc n) (second %))
+                                                    #(vector (first %) (dec n))))
+          non-matches       (update intervals ctg (if (= op \>)
+                                                    #(vector (first %) n)
+                                                    #(vector n (second %))))
+          recursive-matches (split-by-rules non-matches (rest rules))]
+
+      (merge* {next-flow matches} recursive-matches))))
+
+(defn get-all-workflows
+  [input]
+  (->> (partition-by #{""} input)
+       first
+       (map parse-workflow)
+       (apply merge)))
+
+(defn process-one
+  [state all-workflows]
+  (let [workflow  (->> state
+                       keys
+                       (remove #{"A" "R"})
+                       first)
+        rules     (get all-workflows workflow)
+        intervals (get state workflow)]
+    (merge* (dissoc state workflow) (split-by-rules intervals rules))))
+
+
+(defn total-combinations
+  [input]
+  (let [all-workflows (get-all-workflows input)]
+    (->> {"in" (zipmap (seq "xmas") (repeat [1 4000])) "A" []}
+         (iterate-until #(= (set (keys %)) #{"A" "R"})
+                        #(process-one % all-workflows))
+         (->! get "A")
+         (map second)
+         (map #(- (second %) (first %) -1))
+         (partition 4)
+         (map #(apply * %))
+         (apply +))))
+
+
+(->> real-input
+     total-combinations
+     time
+     #_(submit-answer 2))
